2208f6b3ec44840d357ebd3541a333df70e6322c
Беляев Максим <nfsmonstr@gmail.com>
2024-12-12 13:12:10 +0300

    try to fix crash

--- android/src/main/java/com/reactnativeappgallery/AppGalleryModule.java
+++ android/src/main/java/com/reactnativeappgallery/AppGalleryModule.java
@@ -14,40 +14,44 @@ import com.facebook.react.module.annotations.ReactModule;
 
 @ReactModule(name = AppGalleryModule.NAME)
 public class AppGalleryModule extends ReactContextBaseJavaModule implements ActivityEventListener {
-    public static final String NAME = "AppGallery";
+  public static final String NAME = "AppGallery";
 
-    private Promise promise;
+  private Promise promise;
 
-    public AppGalleryModule(ReactApplicationContext context) {
-      super(context);
-      context.addActivityEventListener(this);
+  public AppGalleryModule(ReactApplicationContext context) {
+    super(context);
+    context.addActivityEventListener(this);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+  @ReactMethod
+  public void rate(Promise promise) {
+    this.promise = promise;
+    try {
+      Intent intent = new Intent("com.huawei.appmarket.intent.action.guidecomment");
+      intent.setPackage("com.huawei.appmarket");
+      super.getCurrentActivity().startActivityForResult(intent, 1001);
+    } catch (Exception e) {
+      promise.reject("0", "Huawei AppGallery not installed");
     }
+  }
 
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
-    }
-
-    @ReactMethod
-    public void rate(Promise promise) {
-      this.promise = promise;
-      try {
-        Intent intent = new Intent("com.huawei.appmarket.intent.action.guidecomment");
-        intent.setPackage("com.huawei.appmarket");
-        super.getCurrentActivity().startActivityForResult(intent, 1001);
-      } catch(Exception e) {
-        promise.reject("0", "Huawei AppGallery not installed");
-      }
-    }
-
-    @Override
-    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+  @Override
+  public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+    try {
       this.promise.resolve(resultCode);
+    } catch (NullPointerException e) {
+      e.printStackTrace();
     }
+  }
 
-    @Override
-    public void onNewIntent(Intent intent) {
+  @Override
+  public void onNewIntent(Intent intent) {
 
-    }
+  }
 }
